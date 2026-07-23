6911db574203ddfb4105a074b2aece3fc1d84373
RajanThagunna-29 <thagunnarajan0@gmail.com>
2026-07-23 21:37:25 +0545

    Add files via upload

---
+++ Rajan_Thagunna/lab 5/Curvefit3.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+y = np.array([2,4,8,16,32])
+
+Y = np.log10(y)
+
+n = len(x)
+
+B = (n*np.sum(x*Y)-np.sum(x)*np.sum(Y))/(n*np.sum(x*x)-(np.sum(x))**2)
+A = (np.sum(Y)-B*np.sum(x))/n
+
+a = 10**A
+b = 10**B
+
+print(f"y = {a:.4f}({b:.4f})^x")
+     
---
+++ Rajan_Thagunna/lab 5/curvefit1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 5, 7, 10, 12])
+
+n = len(x)
+
+a = (n*np.sum(x*y) - np.sum(x)*np.sum(y)) / (n*np.sum(x*x) - (np.sum(x))**2)
+b = (np.sum(y) - a*np.sum(x))/n
+
+print("Equation of best fit:")
+print(f"y = {a:.4f}x + {b:.4f}")
+     
